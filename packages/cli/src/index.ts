f9d2c95bf8e3c64e6cb58f7f0ecf2ab4ff141109
cyan0714 <917244295@qq.com>
2024-11-05 17:21:23 +0800

    添加获取git提交记录作为日报命令

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,24 +1,50 @@
 #!/usr/bin/env node
-import create from '@cyan0714-cli/create';
-import { Command } from 'commander';
-import fse from 'fs-extra';
-import path from 'node:path';
+import create from '@cyan0714-cli/create'
+import { Command } from 'commander'
+import fse from 'fs-extra'
+import path from 'node:path'
+import { execSync } from 'child_process'
 
-const pkgJson = fse.readJSONSync(path.join(import.meta.dirname, '../package.json'));
+const pkgJson = fse.readJSONSync(path.join(import.meta.dirname, '../package.json'))
 
-const program = new Command();
+const program = new Command()
 
-console.log('test');
+// 获取今天凌晨的日期字符串
+function getTodayDateString() {
+  const today = new Date()
+  return today.toISOString().split('T')[0] + 'T08:00:00'
+}
+
+program.name('cyan0714-cli').description('脚手架 cli').version(pkgJson.version)
 
 program
-    .name('cyan0714-cli')
-    .description('脚手架 cli')
-    .version(pkgJson.version);
+  .command('create')
+  .description('创建项目')
+  .action(async () => {
+    create()
+  })
 
-program.command('create')
-    .description('创建项目')
-    .action(async () => {
-        create();
-    });
+program
+  .command('gl')
+  .description('获取指定作者的 git 提交记录')
+  .option('-a, --author <name>', '作者名称', 'chenshiyan')
+  .action((options) => {
+    try {
+      const dateStr = getTodayDateString();
+      const command = `git log --format='%s' --since="${dateStr}" --author="${options.author}" | grep -v 'Merge' | awk '{sub(/^[^:]*:/, ""); print NR ". " $0}'`;
+      
+      console.log('查询日期:', dateStr);
+      console.log('查询作者:', options.author);
+      
+      const result = execSync(command, { encoding: 'utf-8' });
+      if (result) {
+        console.log(result);
+      } else {
+        console.log('今天还没有提交记录');
+      }
+    } catch (error) {
+      console.error('执行命令失败:', error);
+    }
+  });
 
-program.parse();
+program.parse()
