1f5d252b9d393e0feacc4a0bb21157ccf5015e2c
cyan0714 <917244295@qq.com>
2024-11-19 17:21:19 +0800

    feat: ejs template

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,6 +1,6 @@
-import { select, input } from '@inquirer/prompts'
+import { select, input, confirm } from '@inquirer/prompts'
 import os from 'node:os'
-import { NpmPackage } from '@guang-cli/utils'
+import { NpmPackage } from '@cyan0714-cli/utils'
 import path from 'node:path'
 import ora from 'ora'
 import fse from 'fs-extra'
@@ -11,11 +11,11 @@ async function create() {
     choices: [
       {
         name: 'react 项目',
-        value: '@guang-cli/template-react',
+        value: '@cyan0714-cli/template-react',
       },
       {
         name: 'vue 项目',
-        value: '@guang-cli/template-vue',
+        value: '@cyan0714-cli/template-vue',
       },
     ],
   })
@@ -25,30 +25,39 @@ async function create() {
     projectName = await input({ message: '请输入项目名' })
   }
 
-  // console.log(projectTemplate, projectName);
+  console.log('projectTemplate', projectTemplate)
 
   const pkg = new NpmPackage({
     name: projectTemplate,
-    targetPath: path.join(os.homedir(), '.guang-cli-template'),
+    targetPath: path.join(os.homedir(), '.cyan0714-cli-template'),
   })
 
   if (!(await pkg.exists())) {
     const spinner = ora('下载模版中...').start()
     await pkg.install()
-    await sleep(3000)
+    await sleep(1000)
     spinner.stop()
   } else {
     const spinner = ora('更新模版中...').start()
     await pkg.update()
-    await sleep(3000)
+    await sleep(1000)
     spinner.stop()
   }
 
   const spinner = ora('创建项目中...').start()
-  await sleep(1000)
 
   const templatePath = path.join(pkg.npmFilePath, 'template')
   const targetPath = path.join(process.cwd(), projectName)
+  console.log('fse.existsSync(targetPath)', fse.existsSync(targetPath));
+
+  if (fse.existsSync(targetPath)) {
+    const empty = await confirm({ message: '该目录不为空，是否清空' })
+    if (empty) {
+      fse.emptyDirSync(targetPath)
+    } else {
+      process.exit(0)
+    }
+  }
 
   fse.copySync(templatePath, targetPath)
 
